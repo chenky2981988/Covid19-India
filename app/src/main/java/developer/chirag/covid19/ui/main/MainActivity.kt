5aaf8880bef24719da6fd725888f4579614686a4
Chiragkumar Sidhiwala <chiragkumar.sidhiwala@kpit.com>
2020-05-28 16:48:18 +0530

    Added Back Button update

--- app/src/main/java/developer/chirag/covid19/ui/main/MainActivity.kt
+++ app/src/main/java/developer/chirag/covid19/ui/main/MainActivity.kt
@@ -2,16 +2,35 @@ package developer.chirag.covid19.ui.main
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.snackbar.Snackbar
+import developer.chirag.covid19.R
 import developer.chirag.covid19.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var activityBinding: ActivityMainBinding;
+    private var backPressTime = 0L
+    private val backPressSnackbar by lazy {
+        Snackbar.make(activityBinding.root, R.string.back_message, Snackbar.LENGTH_SHORT)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         activityBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(activityBinding.root)
 
+    }
+
+
+    override fun onBackPressed() {
+
+        if(backPressTime + 2000 > System.currentTimeMillis()) {
+            backPressSnackbar.dismiss()
+            super.onBackPressed()
+            return
+        } else{
+            backPressSnackbar.show()
+        }
+        backPressTime = System.currentTimeMillis()
     }
 }
